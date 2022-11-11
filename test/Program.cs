bae8e5e3b25af1457bfeb2177b596bca941e4c87
BroadKiller <Gerachenko202@mail.ru>
2022-11-12 00:52:06 +0700

    tru

--- test/Program.cs
+++ test/Program.cs
@@ -7,12 +7,9 @@ namespace Lab4._1
 		
 		static void Main(string[] args)
 		{
-			double a, b, z;
 
-			Console.WriteLine("Введите первое число");
-			a = Convert.ToDouble(Console.ReadLine());
-			Console.WriteLine("Введите второе число");
-			b = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Выберите число: 1. Выполнить сложение. 2. Выполнить вычитание. 3. Выполнить умножение. 4. Выполнить деление");
+			z = Convert.ToInt32(Console.ReadLine());
 
 		}
 	}
